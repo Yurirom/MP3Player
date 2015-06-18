72a65e91aa0cfc32893414c202aeffbb4ab594f5
Yuri Romanov <YURI@Yuri-Air.local>
2015-06-18 07:46:24 +0600

    ver. 1

--- MP3Player/ViewController.swift
+++ MP3Player/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-	override func viewDidLoad() {
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
 	}
 
 	override func didReceiveMemoryWarning() {
