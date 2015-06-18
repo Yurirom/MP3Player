37cd84cd4fa3328869523976ab3de0802d388548
Yuri Romanov <YURI@Yuri-Air.local>
2015-06-18 07:29:55 +0600

    Initial Commit

---
+++ MP3Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MP3Player
+//
+//  Created by Yuri Romanov on 18.06.15.
+//  Copyright (c) 2015 yurirom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
