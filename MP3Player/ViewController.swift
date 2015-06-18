f21688a6ca6e1ddda2c8cf9e4d85dc34b9536559
Yuri Romanov <YURI@Yuri-Air.local>
2015-06-18 08:07:40 +0600

    ver. 2

--- MP3Player/ViewController.swift
+++ MP3Player/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		
+		// Dispose of any resources that can be recreated.
 	}
 
 
